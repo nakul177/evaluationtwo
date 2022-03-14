b9887db07d3c21ea4481e37c8fa934c703530bb1
nakul177 <96129513+nakul177@users.noreply.github.com>
2022-03-14 10:55:49 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {AddHouse} from "./Components/AddHouse/AddHouse.jsx"
+import {Rentals} from "./Components/Rentals/Rentals.jsx"
+import { useState } from 'react';
+
 function App() {
+  const [show , setShow] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App">
+      <button className="toggleForm" onClick={() =>{
+        setShow(!show)
+      }}>
+        {show ? "House Data" :  "From "}
+      </button>
+      {show ? <AddHouse /> : <Rentals />   }
+      <br />
+  
+     
     </div>
   );
 }
---
+++ src/Components/Rentals/Rentals.jsx
@@ -0,0 +1,89 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./Rentals.css";
+
+export const Rentals = () => {
+
+const [ data , setData] = useState([])
+
+useEffect(() =>{
+  axios.get("http://localhost:3001/houses").then((res)=>{
+    setData(res.data)
+  })
+
+},[])
+
+
+
+  return (
+    <div className="rentalContainer">
+      <div className="sortingButtons">
+        <button className="sortById" onClick={data.map((e) =>(
+        e.id
+        )).sort((a,b) =>{
+          return a-b
+        })}>Sort by ID</button>
+        <button className="sortByRentAsc" onClick={data.map((e) =>(
+       e.Rentals
+        )).sort((a,b) =>{
+          return a-b
+        })}>Rent Low to high</button>
+        <button className="sortByRentDesc" onClick={data.map((e) =>(
+       e.rent
+        )).sort((a,b) =>{
+          return b-a
+        })}>Rent High to low</button>
+        <button className="sortByAreaAsc" onClick={data.map((e) =>(
+       e.Rentals
+        )).sort((a,b) =>{
+          return a-b
+        })}>Area Low to high</button>
+        <button className="sortByAreaDesc" onClick={data.map((e) =>(
+       e.Rentals
+        )).sort((a,b) =>{
+          return b - a
+        })}>Area High to Low</button>
+      </div>
+      <input
+        className="searchAddress"
+        type="text"
+        placeholder="Search Address"
+      />
+      <table className="table" border="1">
+        <thead>
+          <tr>
+            <th>Sl.no.</th>
+            <th>Name</th>
+            <th>Owner Name</th>
+            <th>Address</th>
+            <th>Area Code</th>
+            <th>Rent</th>
+            <th>Available For</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((house, index) => {
+            return (
+              <tr key={house.id} className="houseDetails">
+                <td className="houseId">{house.id}</td>
+                <td className="houseName">{house.name} </td>
+                <td className="ownersName">{house.ownerName}</td>
+                <td className="address">{house.address}</td>
+                <td className="areaCode">{house.areaCode}</td>
+                <td className="rent">{house.rent}</td>
+                <td className="preferredTenants">
+                   married:{house.married}
+                   bachelor:{house.bachelor}
+                </td>
+                <td className="houseImage">
+                  <img src={house.image} alt="house" />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
